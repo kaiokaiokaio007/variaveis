45681bef0697b4db6e912e58260ac5611b970823
kaio everton <165791089+kaiokaiokaio007@users.noreply.github.com>
2024-08-30 07:36:20 -0300

    Add files via upload

---
+++ variaveis/script.js
@@ -0,0 +1,36 @@
+function calculate() {
+    
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 + num2;
+
+    document.getElementById('result').textContent = resultado;
+}
+
+function menos() {
+    
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 - num2;
+
+    document.getElementById('result').textContent = resultado;
+}
+
+function dividir() {
+    
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 / num2;
+
+    document.getElementById('result').textContent = resultado;
+}
+
+function multiplicar() {
+    
+    var num1 = parseFloat(document.getElementById('num1').value);
+    var num2 = parseFloat(document.getElementById('num2').value);
+    var resultado = num1 * num2;
+
+    document.getElementById('result').textContent = resultado;
+}
+
